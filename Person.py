0615080cb58d7db2c30d5f55aa7517ffb6a34ae2
Casey <casey@local>
2021-11-06 18:50:13 -0700

    add wallrunner.py wall and person are loading

--- Person.py
+++ Person.py
@@ -12,7 +12,7 @@ class Person:
     def __init__(self, newX, newY):
         self.x = newX
         self.y = newY
-        self.img = pygame.image.load("wall.gif")
+        self.img = pygame.image.load("dude.gif")
     
         
     # draw the image on the surface
