6472f9e432818dbe38421bc37c6576ba42dd9f45
Casey <casey@local>
2021-11-10 11:37:15 -0700

    new

---
+++ sum_digits.py
@@ -0,0 +1,11 @@
+#(c) A+ Computer Science
+#www.apluscompsci.com
+
+def go( num ):
+  return 0
+    
+    
+while ( True ):
+    # enter a number
+    val = int(input("Enter a number :: "))
+    print( go( val ) )
