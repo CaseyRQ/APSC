9cb7b3d442b79432f40696b2ca227c5296a9138d
Casey <casey@local>
2021-11-15 15:59:27 -0700

    new count odds

---
+++ count_odds.py
@@ -0,0 +1,19 @@
+#(c) A+ Computer Science
+#www.apluscompsci.com
+
+def go( num ):
+  return 0
+    
+    
+print( go( 234 ) )
+print( go( 10000 ) )
+print( go( 111 ) )
+print( go( 9005 ) )
+print( go( 84645 ) )
+print( go( 8547 ) )
+print( go( 123456789 ) )
+print( go( 5555672468 ) )
+print( go( 2548522125455 ) )
+print( go( 2545588514548 ) )
+print( go( 111111111 ) )
+print( go( 121212121212 ) )
