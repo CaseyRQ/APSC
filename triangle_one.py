eded285600e04d6b72a948d491b414c26a67614d
Casey <casey@local>
2021-11-17 14:03:53 -0700

    fin

--- triangle_one.py
+++ triangle_one.py
@@ -2,12 +2,14 @@
 #www.apluscompsci.com
 
 def go( num ):
-  print(" ")
-    
+  for row in range(num):
+    for col in range(row):
+      print("#",end = " ")
+    print(" ")    
     
 go( 1 ) 
 go( 2 ) 
 go( 3 ) 
 go( 4 ) 
-go( 9 ) 
+go( 9  ) 
 go( 12 ) 
