7defdd72d4c8a54746281e29acd3de99cdf5e2d5
Casey <casey@local>
2021-11-18 14:02:46 -0700

    grass

--- Pygame_Frogger/Ground.py
+++ Pygame_Frogger/Ground.py
@@ -20,17 +20,19 @@ def draw(window):
     
     # Outer loop runs as long as x is 
     # not past the right edge of the screen
-    
+    while xPos < 800:
+        
         # Inner loop runs as long as y is 
         # not past the bottom edge of the screen
-        
+        while yPos < 600:
             # Draws the grass and increases the y
-    window.blit(grass, (xPos,yPos))            
-            
+            window.blit(grass, (xPos,yPos))            
+            yPos += 50   
 
             
         # Increase the x and reset the y
-        
+        xPos += 50
+        yPos = 0
     
     
     # Draw 30 rocks in random x and y positions
