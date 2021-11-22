8bfe5115c9a92d59aff40b1613157efd7578077e
Casey <casey@local>
2021-11-22 13:47:41 -0700

    new

---
+++ Pygame_Frogger/timer.py
@@ -0,0 +1,12 @@
+import pygame
+
+# Ask user for number of seconds
+
+
+
+# Loop while seconds are less or equal to 0
+
+    # Part 1 - Print the time left as seconds
+    
+    # Part 2 - Print the time left as minutes and seconds
+    
