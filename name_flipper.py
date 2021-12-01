54f4bc69a068a80c7b9d0b131c11b61780bb00eb
Casey <casey@local>
2021-12-01 11:15:08 -0700

    .

---
+++ name_flipper.py
@@ -0,0 +1,16 @@
+#(c) A+ Computer Science
+#www.apluscompsci.com
+
+def flip_em( s ):
+  #add in code to flip the names
+  #and add in a comma between the flipped names
+  return "add code here"
+
+print ( flip_em("aplus comp") )
+print ( flip_em("comp aplus") )
+print ( flip_em("ben sam") )
+print ( flip_em("sally sue") )
+print ( flip_em("big man") )
+print ( flip_em("fat head") )
+
+
