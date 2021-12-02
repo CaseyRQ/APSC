649ae82864251ebfacc43773a2c3e64ffc3bccd3
Casey <casey@local>
2021-12-02 13:35:28 -0700

    .

---
+++ first.py
@@ -0,0 +1,18 @@
+# (c) A+ Computer Science
+# www.apluscompsci.com
+
+def go( word ):
+  return "code";
+
+
+print ( go( "dog#cat#pigaplus" ) )
+print ( go( "pigs#apluscompsci#food" ) )
+print ( go( "a" ) )
+print ( go( "aplus" ) )
+print ( go( "01234567890" ) )
+print ( go( "abcdefghijklmnopqrstuvwxyz" ) )
+print ( go( "code#H00P#code1234" ) )
+print ( go( "##wowgira77##eplus" ) )
+print ( go( "catsandaplusdogsaplus###" ) )
+print ( go( "7" ) )
+
