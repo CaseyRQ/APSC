072544ebe8ea8a661e467c140c22a73a6ccab0c3
Casey <casey@local>
2021-12-02 14:14:08 -0700

    .

---
+++ upper_lower.py
@@ -0,0 +1,23 @@
+# (c) A+ Computer Science
+# www.apluscompsci.com
+
+def go( a, b ):
+  return "add code here"
+
+
+print ( go( "D0g", True ) )
+print ( go( "aplus", False ) )
+print ( go( "catgiraffe", False ) )
+print ( go( "ApLuS", True ) )
+print ( go( "ApLuS", False ) )
+print ( go( "#d0G# ", True ) )
+print ( go( "#d0G# ", False ) )
+print ( go( "aplusdog#13337#", False ) )
+print ( go( "CoEe1234", True) )
+print ( go( "wow", True ) )
+print ( go( "77777" , True ) )
+print ( go( "77777" , False ) )
+
+
+
+
