d5ea87fa0fd36512c0d09cae12f913677611ad6b
Casey <casey@local>
2021-12-02 07:57:19 -0700

    .

---
+++ avg_len_2.py
@@ -0,0 +1,21 @@
+# (c) A+ Computer Science
+# www.apluscompsci.com
+
+def go( a, b ):
+  return 0
+
+
+print ( go( "dog", "cats" ) )
+print ( go( "aplus", "pigs" ) )
+print ( go( "catgiraffe", "apluscompsci" ) )
+print ( go( "ap", "aplus" ) )
+print ( go( "pluscat", "dogsaplus" ) )
+print ( go( "#", "#" ) )
+print ( go( "aplusdog#13337#", "pigaplusprogram" ) )
+print ( go( "code", "code1234" ) )
+print ( go( "wow", "eplus" ) )
+print ( go( "catsand" , "aplusdogsaplus" ) )
+print ( go( "7", "77777" ) )
+
+
+
