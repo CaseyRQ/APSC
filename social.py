cd2ec563064594fafdb9441e8ebe65fd878ba591
Casey <casey@local>
2021-12-03 13:28:59 -0700

    .

---
+++ social.py
@@ -0,0 +1,26 @@
+# (c) A+ Computer Science
+# www.apluscompsci.com
+
+def go( word ):
+  return "code"
+
+
+print ( go( "463-44-5678" ) )
+print ( go( "46-144-5678" ) )
+print ( go( "111-44-5678" ) )
+print ( go( "463044-5678" ) )
+print ( go( "463-99-8888" ) )
+print ( go( "123-11-5323" ) )
+print ( go( "463-4-55678" ) )
+print ( go( "46314415678" ) )
+
+
+
+
+
+
+
+
+
+
+
