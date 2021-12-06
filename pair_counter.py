e10e9a2c972dca9cb7c5826a6f33f56e7f734663
Casey <casey@local>
2021-12-06 13:22:17 -0700

    .

---
+++ pair_counter.py
@@ -0,0 +1,25 @@
+#(c) A+ Computer Science
+#www.apluscompsci.com
+
+def count_pairs( s ):
+  #add in code to count 
+  #how many pairs of letters exist
+  #return the number of letter pairs in each string
+  #aadogbbcatcc would return 3
+  #aadogcatcc would return 2
+  return 0
+
+print ( count_pairs("ddogccatppig") )
+print ( count_pairs("dogcatpig") )
+print ( count_pairs("xxyyzz") )
+print ( count_pairs("a") )
+print ( count_pairs("abc") )
+print ( count_pairs("aabb") )
+print ( count_pairs("dogcatpigaabbcc") )
+print ( count_pairs("aabbccdogcatpig") )
+print ( count_pairs("dogabbcccatpig") )
+print ( count_pairs("aaaa") )
+print ( count_pairs("AAAAAAAAA") )
+
+
+
