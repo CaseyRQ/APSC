7e36bf7b19f8a3b9863f8ae8cefb494227ffac0e
Casey <casey@local>
2021-12-09 13:30:01 -0700

    .

--- Graphing.py
+++ Graphing.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
-def graph_distance(distance, time):
+def graph_speed(distance, time):
 x = 
-y = x**2
+y = 
 plt.colorbar()
 plt.plot(x,y + 5)
 plt.show()
