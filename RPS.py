eedb4cd8b587d447ddf431d6139cf41f3b8d9938
Casey <casey@local>
2021-12-09 18:04:19 -0700

    .

--- RPS.py
+++ RPS.py
@@ -1,18 +1,35 @@
 
-
 import random
-Player = input("Enter a choice (rock, paper, scissors):").lower
+
+Player = input("Enter a choice (rock, paper, scissors):").lower()
 choices = ["rock", "paper", "scissors"]
 Comp = random.choice(choices)
 
+
 if Player == Comp:
-        print("Both players chose" (Player), "It's a tie")
-elif Player == "rock" and Comp == "scissors":
-        print ("Player wins!")
-if Player == "paper" and Comp == "scissors":
-        print ("Comp wins!")
-elif Player == "scissors" and Comp == "rock":
-        print("Comp wins!")
+        print("Both players chose", (Player), "It's a tie.")
+elif Player == "rock":
+        if Comp == "scissors":
+                print ("Player wins!")
+        else: print("Comp wins")
+if Player == "paper":
+        if Comp == "rock":
+                print("Player wins!")
+        else: print("Comp wins!")
+elif Player == "scissors":
+        if Comp == "rock":
+                print("Comp wins!")
+        else: print("Player wins!")
+
+Compscore = 0
+Playerscore = 0
+
+while Player and Comp == True:
+        if print ("Player wins!"):
+                Playerscore += 1
+                pass; Playerscore 
+        pass; Compscore + 1
+
 
 
 
