778435269bcf78b5f1c418bea445cab40b854a16
Casey <casey@local>
2022-01-11 13:44:29 -0700

    .

--- Ground.py
+++ Ground.py
@@ -8,14 +8,18 @@ def drawGround(window):
     """
     load all images into a list 
     """
-    
+    images = [pygame.image.load("grass.gif").convert(), 
+                       pygame.image.load("gtoroad.gif").convert(),
+                       pygame.image.load("road.gif").convert(), 
+                       pygame.image.load("rtograss.gif").convert()]
     
     
     """
     Create variables to keep track of where to
     draw the grass
     """
-    
+    yPos = 0
+    xPos = 0
     
     
     
@@ -35,9 +39,17 @@ def drawGround(window):
     Use the y position to determine which tile to draw
     Increase the y
     """
-    
+    while yPos < 600:
+        if yPos <= 50:
+            images(images[0], xPos,yPos)
+        elif yPos > 50 and yPos < 60:
+            images(images[1], xPos,yPos)
+        elif yPos > 60 and yPos < 500: 
+            images(images[2], xPos,yPos)
+        elif yPos > 500 or yPos < 600:
+            images(images[3], xPos,yPos)
         
     """
     Increase the x and reset the y
     """
-        
+    
