ea4fdebe65e1844a88e4d6558a05294907167e1d
Casey <casey@local>
2022-01-14 14:13:55 -0700

    .

---
+++ algo_biggest.py
@@ -0,0 +1,19 @@
+# (c) A+ Computer Science
+# www.apluscompsci.com
+
+def check( ray ):
+    #write the code to loop through ray
+       #return the biggest number in ray
+    return -1
+            
+things = [56,65,98,2,25,99,-110]
+print( check( things ) )
+
+things = [1]
+print( check( things ) )
+
+things = [1,2,3,4,5,6]
+print( check( things ) )
+
+things = [11,21,3,4,15,6]
+print( check( things ) )
