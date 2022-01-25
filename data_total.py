37c7ebb7000aa1f35f533536019d2821f0a600eb
Casey <casey@local>
2022-01-25 07:24:13 -0700

    .

--- data_total.py
+++ data_total.py
@@ -1,1 +1,9 @@
 #build the program to read in all of the values in the file and total them up
+
+file = open('input.dat','r')
+number = int(file.readline().strip())
+sum = 0
+for n in range(0,number):
+    readline = int(file.readline().strip())
+    sum += readline
+print (sum)
