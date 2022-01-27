6ee680152e47e5da6be2b1dc2cfae66a30b0f31f
Casey <casey@local>
2022-01-27 13:34:00 -0700

    .

--- baby_names_file_input.py
+++ baby_names_file_input.py
@@ -3,25 +3,25 @@
 #load in the yobXXXX.txt file for the year
 #you want to analyze
 #or load in the state file you want to analyze
-file = open('yob1970.txt', 'r')
+file = open('yob2004.txt', 'r')
 
 #variables to store the most popular name
 #and the most popular name count
-most_pop_name = ""
-max = float('-inf')
+c_names = 0
+
 
 for line in file:
     #split around commas [,] in the data file 
     list = line.split(',')
     
     #get the count for the current name
-    times = 0
     
-    #find the name that was most popular
-    
-    #add in code here to find most popular name
+    #find the names that start with C 
+    #add in code here to find C names
+    if line[0]=='C' and list[1]=='M':
+        c_names += 1
     
 
 #print most popular name
-print( "Most popular name in [year] :: " + most_pop_name + " " + str(max))    
+print ("Amount of names that start with C in 2004 for males are  :: ", c_names   )  
     
