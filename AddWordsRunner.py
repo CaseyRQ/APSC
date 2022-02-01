e557e82d27bdbef5a787d10530eaa41aef1def53
Casey <casey@local>
2022-02-01 13:24:12 -0700

    .

---
+++ AddWordsRunner.py
@@ -0,0 +1,9 @@
+# (c) A+ Computer Science
+# www.apluscompsci.com
+
+from HelperMethods import *
+from WordCounter import *
+
+import random
+
+# Write a loop that creates and prints 25 WordCounters
