9e9ffb2f308c80054e7f3deff993338910ec1ad3
Casey <casey@local>
2022-02-01 13:47:13 -0700

    .

--- AddWordsRunner.py
+++ AddWordsRunner.py
@@ -1,9 +1,16 @@
 # (c) A+ Computer Science
 # www.apluscompsci.com
 
+from calendar import c
+from tkinter import W
 from HelperMethods import *
 from WordCounter import *
 
 import random
 
 # Write a loop that creates and prints 25 WordCounters
+for x in range(25):
+    wc = (WordCounter((makeNonsenseWord()), random.randint(1,45)))
+    print (wc)
+
+    
