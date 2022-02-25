8348de7f8d60d233bf72abbb691231d5de81417c
Casey <casey@local>
2022-02-25 07:57:30 -0700

    .

--- magic8ball.py
+++ magic8ball.py
@@ -1,28 +1,22 @@
+#!/usr/bin/env python3
+import random
 
-from curses import KEY_BACKSPACE
-from random import random
-
-while True:
-    question = (input("Ask a yes or no question. Press 'delete' to quit."))
-    if KEY_BACKSPACE:
-        break
-    print("Goodbye.")
-    answers = random(1,8)
-    if answers == 1:
-        print ("Yes")
-    elif answers == 2:
-        print("No")
-    elif answers == 3:
-        print("Maybe")
-    elif answers == 4:
-        print("Very doubtful")
-    elif answers == 5:
-        print ("Ask again later")
-    elif answers == 6:
-        print("Definetly")
-    elif answers == 7:
-        print("Cannot predict now")
-    elif answers == 8:
-        print("Most likely")
-    if question != "":
-        print(answers)
+def magicball(): 
+        question = (input("Ask a yes or no question. To quit, type quit:\n"))
+        answers = [
+            "Yes", 
+            "No",
+            "Maybe",
+            "Very doubtful",
+            "Ask again later",
+            "Definetly",
+            "Cannot predict now",
+            "Most likely"]
+        if question.lower() == "quit":
+            print ("Goodbye.")
+        else:
+            question != "How" or "Why"
+            print(random.choice(answers))
+   
+        
+magicball()
